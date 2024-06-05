afba1c737fcef4f09076cafe70f9e55f3be86cc9
Naitik59 <123869903+Naitik59@users.noreply.github.com>
2024-06-05 00:06:45 -0400

    initial commit
    initial commit

---
+++ Naitik Patel Lab-5/Naitik Patel Lab-5/ViewController1.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Naitik Patel Lab-5
+//
+//  Created by Naitik Ratilal Patel on 04/06/24.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Naitik Patel Lab-5/Naitik Patel Lab-5/ViewController6.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController6.swift
+//  Naitik Patel Lab-5
+//
+//  Created by Naitik Ratilal Patel on 04/06/24.
+//
+
+import UIKit
+
+class ViewController6: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
